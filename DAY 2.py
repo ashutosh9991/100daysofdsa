5420327224f420427d6b2a86984a31c86a0c6f25
ashutosh9991 <76027357+ashutosh9991@users.noreply.github.com>
2021-03-02 21:29:08 +0530

    DAY 2 insertion sort

---
+++ DAY 2.py
@@ -0,0 +1,15 @@
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+
+
+arr=[13,14,2,3,10,9]
+insertionsort(arr)
+for i in range(len(arr)):
+    print("% d" % arr[i])
